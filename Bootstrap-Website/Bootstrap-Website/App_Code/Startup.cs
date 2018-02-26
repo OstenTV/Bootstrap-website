0111b8b4f7b383023bfad9fabddb1b9f7d95783c
OstenTV <info@ostentv.dk>
2018-02-26 10:18:07 +0100

    Created things.

---
+++ Bootstrap-Website/Bootstrap-Website/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bootstrap_Website.Startup))]
+namespace Bootstrap_Website
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
